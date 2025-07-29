3266262294e9d3275bb61e94262e5502cfb8294f
azafrastore <azafrastore@gmail.com>
2025-07-29 11:28:50 -0500

    Actualizacion catalogo

---
+++ actualizar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Agregar archivos al staging
+git add .
+
+# Hacer commit con el mensaje proporcionado
+git commit -m "Actualizacion catalogo"
+
+# Hacer push a la rama actual
+git push
